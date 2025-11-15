e3b098b6f1f3b1ebf71ca667a7c29dde666acbee
Emirhan Durmuş <emirhan.durmus1@gmail.com>
2025-11-15 18:51:04 +0300

    Add Author model and update Book model relationships; initialize Alembic configuration

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base() 
@@ -12,10 +13,20 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     
 
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    biography = Column(String, index=True)
+    
+    
+
 class Book(Base):
     __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    author = Column(String, index=True)
-    description = Column(String, index=True)
+    author_id = Column(Integer, index=True)
+    
+    author = relationship("Author", back_populates="books")
