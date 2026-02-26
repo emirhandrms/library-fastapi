550175173e9f0e35d491ea6ec35b09f467a3b340
Emirhan Durmuş <emirhan.durmus1@gmail.com>
2026-02-26 11:07:24 +0000

    remove alembic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.routers import books
+from app.db.service import engine
+from app.db.models import Base
 
 
 app = FastAPI(title="Books API (Mock DB)",)
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 app.include_router(books.router)
