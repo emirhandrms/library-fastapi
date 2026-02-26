c2b9449977bed43ce9be37514db16251f2dca85e
Emirhan Durmuş <emirhan.durmus1@gmail.com>
2026-02-26 10:27:21 +0000

    sql connection string

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,7 +1,10 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 class Settings(BaseSettings):
-    SQL_CONNECTION_STRING: str
-    
+    SQL_CONNECTION_STRING: str = Field(..., env="SQL_CONNECTION_STRING")
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
